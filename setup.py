ecb7abc6b9a2521efb5d5fa03f726efa2ca03896
RA341 <razerishi1505@gmail.com>
2024-03-05 15:27:54 -0500

    license and setup

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="your-package-name",
+    name="audicium-extension-base",
     version="0.1",
     packages=find_packages(),
-    description="Description of your package",
-    author="Your Name",
-    author_email="your.email@example.com",
-    url="https://github.com/your-username/your-package",
+    description="Base classes and models to create an audicium extension",
+    author="RA341",
+    author_email="",
+    url="https://github.com/RA341/audicium_extension_base",
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
